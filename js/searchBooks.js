3d5e9bf7f5100114522b2f676c5e1871168d6f7f
Gary James <gjames@edgetechacademy.edu>
2020-12-22 12:46:49 -0600

    added in a Crypto Currentcy app

--- js/searchBooks.js
+++
@@ -1,5 +0,0 @@
-fetch('https://www.googleapis.com/books/v1/volumes?q=harry%20potter')
-.then(resp => resp.json())      //  wait for the response and convert it to JSON
-.then(books => {                //  with the resulting JSON data do something
-    var list = books.items.map(b => b.volumeInfo.title)
-});
