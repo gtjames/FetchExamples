ff6201785b62eb556e720fe584864ec994b4a3de
gtjames <gtjames@gmail.com>
2022-03-09 19:32:10 -0600

    added in an API for translation

--- js/translate.js
+++ js/translate.js
@@ -1,18 +1,22 @@
-fetch("https://google-translate1.p.rapidapi.com/language/translate/v2/detect", {
-	"method": "POST",
-	"headers": {
-		"content-type": "application/x-www-form-urlencoded",
-		"accept-encoding": "application/gzip",
-		"x-rapidapi-host": "google-translate1.p.rapidapi.com",
-		"x-rapidapi-key": "498ed225bamshcd02cf5559e10edp179d21jsn59b140b93ec5"
-	},
-	"body": {
-		"q": "English is hard, but detectably so"
-	}
-})
-	.then(response => {
-		console.log(response);
-	})
-	.catch(err => {
-		console.error(err);
-	});
+import axios from "axios";
+
+const options = {
+  method: 'POST',
+  url: 'https://lecto-translation.p.rapidapi.com/v1/translate/text',
+  headers: {
+    'content-type': 'application/json',
+    'x-rapidapi-host': 'lecto-translation.p.rapidapi.com',
+    'x-rapidapi-key': '498ed225bamshcd02cf5559e10edp179d21jsn59b140b93ec5'
+  },
+  data: {
+    texts: ['Just try it mate.', 'What are you waiting for?'],
+    to: ['hi'],
+    from: 'en'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
