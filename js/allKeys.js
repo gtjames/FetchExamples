cf60b8d981e36042a5440f01d382799d2591326f
gtjames <gtjames@gmail.com>
2024-05-08 22:32:59 -0700

    update allKeys with empty keys

---
+++ js/allKeys.js
@@ -0,0 +1,12 @@
+let keyCanvas       =   '';
+let keyDictionary   =   '';
+let keyGoogleMaps   =   '';
+let keyNASA         =   '';
+let keyNatlParks    =   '';
+let keyNews         =   '';
+let keyOmdb         =   '';
+let keyOpenWX       =   '';
+let keyWebcams      =   '';
+let keyWorldArt     =   '';
+
+let keyRapidAPI     =   '';
