f3965482b70c02135f13fd6fc056eb4fb211f402
gtjames <gtjames@gmail.com>
2024-10-08 19:50:23 -0500

    reduced the keys

--- js/allKeys.js
+++ js/allKeys.js
@@ -1,15 +1,15 @@
-let keyCanvas       =   '10706~1r7OxhF6t3ksuV4w8HVWN6e4LYwcYuXaqLl3pC0cH9TKcll6NEYDxYOi6thAm1Z2';
-let keyDictionary   =   '3f979797-60ca-48d8-a9e8-10d05fdd77f8';
-let keyGoogleMaps   =   'AIzaSyAnfQpXQJx_S10A-5wyuRPZGlBpXhKD7zk';
-let keyNASA         =   'Aw0TZ7aE7e6WJnh4t7plOXEk1xdbCg45NMqfUX42';
-let keyNatlParks    =   'g56CN1WcTJ7QfTf6FTkDH14cCgeeT0UPFLlXubUr';
-let keyNews         =   '04ea6452dc4a86b9ee13f2ab289bdc38';
-let keyOmdb         =   '2c791b47';
-let keyOpenWX       =   'a099a51a6362902523bbf6495a0818aa';               
-let keyWebcams      =   'vAu65qnE3oXk3CtPQbI7YX9JURUOjdfb';
-let keyWorldArt     =   'gperglagenti';
+let keyCanvas       =   '1070';
+let keyDictionary   =   '3f97';
+let keyGoogleMaps   =   'AIza';
+let keyNASA         =   'Aw0T';
+let keyNatlParks    =   'g56C';
+let keyNews         =   '04ea';
+let keyOmdb         =   '2c79';
+let keyOpenWX       =   'd069';
+let keyWebcams      =   'vAu6';
+let keyWorldArt     =   'gper';
 //	used by multiple apps
-let keyRapidAPI     =   '498ed225bamshcd02cf5559e10edp179d21jsn59b140b93ec5';
+let keyRapidAPI     =   '498e';
 
 let courses = [
     {course: "295338", institution: "byui", name: "WDD 330-S24" },
@@ -18,9 +18,12 @@ let courses = [
     {course: "274380", institution: "byui", name: "WDD 330-F23" },
     {course: "99424",  institution: "unt",  name: "CSCE 2610-W23" },
     {course: "91018",  institution: "unt",  name: "CSCE 3420-S24" },
+    {course: "110652", institution: "unt",  name: "CSCE 3600-W24" },
+    {course: "110534", institution: "unt",  name: "CSCE 1015-W24" },
+    {course: "110XXX", institution: "unt",  name: "CSCE 1015-W24" },
 ];
 
 let keys = {
-    byui: "10706~1r7OxhF6t3ksuV4w8HVWN6e4LYwcYuXaqLl3pC0cH9TKcll6NEYDxYOi6thAm1Z2",  
-    unt:   "9082~2eACOSJfc50T8CF0xuxjgPzihp0QW6qS9MkMMAhM3vvLmN1S4LLa4A3Bsd1c3Ko2"
+    byui: "1070",
+    unt:  "9082"
 }
